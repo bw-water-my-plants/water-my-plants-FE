ac8102d6bcc6b8629be15f0681e40e301d5edc6a
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-24 18:07:53 +0100

    Start styled components for cards

---
+++ water-plants/src/components/PlantList/PlantCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import Colors from '../../styling/colors';
+
+const Card = styled.div`
+    width: 90vw;
+    height: 20vw;
+    background: ${Colors.Light};
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    display: flex;
+    justify-content: space-between;
+`;
+
+export default function PlantCard(props) {
+    
+}
---
+++ water-plants/src/styling/colors.js
@@ -0,0 +1,8 @@
+const Colors = {
+    Primary: '#04BF68',
+    Secondary: '#02733E',
+    Tertiary: '#038C25',
+    Failure: '#734C08',
+    Light: '#F2F2F2',
+}
+export default Colors; 
