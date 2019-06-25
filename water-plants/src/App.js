b6dcb40f48ef7c254ed54d3fca017e3cb1bed022
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-25 08:41:19 +0100

    Add Card structure

--- water-plants/src/App.js
+++ water-plants/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Provider } from "react-redux";
 import "./App.css";
 import  store  from "./store"
-
+import Plant from './components/PlantList/PlantCard';
 function App() {
   return (
     <Provider store = {store}>
       <div className="App">
         <h1>HELLO WORLD</h1>
+        <Plant/>
       </div>
     </Provider>
   );
--- water-plants/src/components/PlantList/PlantCard.jsx
+++ water-plants/src/components/PlantList/PlantCard.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
 import Colors from '../../styling/colors';
-
-const Card = styled.div`
+import exampleImage from '../../../assets/images/example_plant.jfif';
+const Card = styled.li`
     width: 90vw;
     height: 20vw;
     background: ${Colors.Light};
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     display: flex;
     justify-content: space-between;
+    padding: 0;
 `;
 
+const PlantProfile = styled.div`
+    flex-grow: 3;
+    display: flex;
+    justify-content: flex-start;
+
+    &::after {
+        height:85%;
+        width:1;
+        border-right: 1px solid black;
+        opacity: 0.1;
+    }
+`;
+
+const InfoBox = styled.div`
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const PlantImage = styled.img`
+    width: 20vw;
+    height: 20vw;
+`;
 export default function PlantCard(props) {
-    
+    return(
+        <Card>
+            <PlantProfile>
+                <PlantImage src={exampleImage} alt="Plant"/>
+            </PlantProfile>
+
+            <InfoBox>
+
+            </InfoBox>
+        </Card>
+    )
 }
