c630d94a5b8dd7fd1b1e90172e0aae4f33c2c2a6
oloruntobi <awoderuoloruntobi@gmail.com>
2019-06-25 07:17:04 +0100

    Created a redux boilerplate using a simple counter

--- water-plants/src/App.js
+++ water-plants/src/App.js
@@ -1,13 +1,39 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./App.css";
-import  store  from "./store"
+import ShowComponent from "./component/ShowComponent"
+
+
+
+const initialState = {
+  count: 0
+}
+const counterReducer = (state = initialState, action) => {
+    switch(action.type){
+      case 'INCREMENT':
+      return {
+        ...state,
+        count: state.count + 1
+      } 
+      case "DECREMENT": 
+      return {
+        ...state,
+        count: state.count - 1
+      }
+      default:
+      return state;
+    }
+}
+
+const store = createStore(counterReducer);
 
 function App() {
   return (
-    <Provider store = {store}>
+    <Provider store={store}>
       <div className="App">
         <h1>HELLO WORLD</h1>
+        <ShowComponent />
       </div>
     </Provider>
   );
