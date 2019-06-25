1ea2d7a5c2860206b28e45d647ba2a98e491792e
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-25 13:14:22 +0100

    Add API reducer

--- water-plants/src/reducers/index.js
+++ water-plants/src/reducers/index.js
@@ -9,13 +9,64 @@ const apiState = {
     fetching: false,
     posting: false,
     loggingIn: false,
-    loginFail: '',
+    loginFail: null,
+    error: null,
+    loggedIn: false,
 }
 
 export function plantsReducer(state = plantState, action) {
     switch(action.type) {
         case(types.SET_PLANTS):
             return { ...state, plants: action.payload };
+        default:
+            return state;
+    }
+}
+
+export function apiReducer(state = apiState, action) { 
+    switch(action.type) {
+        case(types.FETCHING_PLANTS):
+            return { ...state, fetching: true };
+        case(types.FETCH_SUCCESS):
+            return {
+                ...state,
+                fetching: true,
+                error: null,
+            };
+        case(types.API_ERROR):
+            return {
+                ...state,
+                fetching: false,
+                posting: false,
+                error: action.payload,
+            };
+        case(types.POST_SUCCESS):
+            return {
+                ...state,
+                posting: false,
+                error: null,
+            };
+        case(types.LOGGING_IN):
+            return {
+                ...state,
+                loggedIn: false,
+                loggingIn: true,
+                loginFail: false,
+            };
+        case(types.LOGIN_SUCCESS):
+            return {
+                ...state,
+                loggedIn: true,
+                loggingIn: false,
+                loginFail: false,
+            };
+        case(types.LOGIN_FAIL):
+            return {
+                ...state,
+                loggedIn: false,
+                loggingIn: false,
+                loginFail: true,
+            }
         default:
             return state;
     }
