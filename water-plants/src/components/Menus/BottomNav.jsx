c4987f7183446e74c3a54302ff0d0fc021ae43ea
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-26 16:48:43 +0100

    Basic BottomNav implementation for testing

--- water-plants/src/components/Menus/BottomNav.jsx
+++ water-plants/src/components/Menus/BottomNav.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Colors from '../../styling/colors';
+import { XIcon } from '../Vectors/Elements';
 import { connect } from 'react-redux';
 
 const Bar = styled.nav`
@@ -12,12 +13,22 @@ const Bar = styled.nav`
     display: flex;
     justify-content: space-around;
 `;
+
+const NavButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: none;
+    border: none;
+`;
 export class BottomNav extends React.Component {
 
     render() {
         return (
             <Bar>
-                
+                <NavButton>
+                    <XIcon color='white' svgHeight='70%' />
+                </NavButton>
             </Bar>
         );
     }
