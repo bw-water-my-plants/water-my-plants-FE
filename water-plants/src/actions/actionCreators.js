f72c354dd75508405309208ad9bacf8fcef197fb
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-26 08:24:07 +0100

    Add action dispatcher for adding plant

--- water-plants/src/actions/actionCreators.js
+++ water-plants/src/actions/actionCreators.js
@@ -35,6 +35,18 @@ export const fetchPlants = () => dispatch => {
         });
 }
 
+export const addPlant = (data) => dispatch => {
+    return axiosWithAuth()
+        .post(CreateAPIUrl('plants'), data)
+        .then(res => {
+            dispatch({ type: types.POST_SUCCESS });
+            dispatch(fetchPlants());
+        })
+        .catch(error => {
+            dispatch(setError(error.message));
+        });
+}
+
 export const login = (email, password) =>  dispatch => {
     dispatch({ type: types.LOGGING_IN });
     return axios
