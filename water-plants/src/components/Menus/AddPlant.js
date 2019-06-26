fd4db86784d50a560f897b627627b7500611e0d2
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-26 20:14:10 +0100

    Add basic JSX and styling to Plant Form

--- water-plants/src/components/Menus/AddPlant.js
+++ water-plants/src/components/Menus/AddPlant.js
@@ -10,4 +10,45 @@ const Container = styled.div`
     flex-direction: column;
     justify-content: space-around;
     align-items: center;
+    padding: 3rem 0;
+    font-family: 'Roboto', sans-serif;
+    position: fixed;
+    bottom: 4.5rem;
 `;
+
+const PlantForm = styled.form`
+    width: 80%;
+    input{
+        background: none;
+        border: none;
+        font-size: 1.6rem;
+        border-bottom: 1px solid black;
+        width: 80%;
+        height: 2rem;
+    }
+`;
+
+export function AddPlant(props) {
+
+    return (
+        <Container>
+            <PlantForm>
+                <h2>Name</h2>
+                <input
+                    type='text'
+                    name='name'
+                />
+                <h2>Type</h2>
+                <input
+                    type='text'
+                    name='type'
+                />
+                <h2>Height</h2>
+                <input
+                    type='number'
+                    name='height'
+                />
+            </PlantForm>
+        </Container>
+    );
+}
