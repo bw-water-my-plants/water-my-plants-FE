6a7ee1de27a7d551a57e6405c82d4a6590cb4ce7
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-26 16:33:54 +0100

    Start BottomNav component

---
+++ water-plants/src/components/Menus/BottomNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import Colors from '../../styling/colors';
+import { connect } from 'react-redux';
+
+export class BottomNav extends React.Component {
+
+    render() {
+        return (
+
+        );
+    }
+}
