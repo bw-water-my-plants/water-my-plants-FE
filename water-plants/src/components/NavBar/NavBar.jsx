b3510f8e91382563fc0eba5b5261016ed7e1175e
oloruntobi <awoderuoloruntobi@gmail.com>
2019-06-27 20:29:37 +0100

    Added logo

--- water-plants/src/components/NavBar/NavBar.jsx
+++ water-plants/src/components/NavBar/NavBar.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-import "./NavBar.css"
+import "./NavBar.css";
 import { Link } from "react-router-dom";
 
 class NavBar extends React.Component {
-    toggle = () => {
-        let x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-          x.className += " responsive";
-        } else {
-          x.className = "topnav";
-        }
-      }
+  toggle = () => {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  };
   render() {
- 
     return (
       <div className="topnav" id="myTopnav">
-        <Link href="#home" className="active">
-          Home
+        <Link >
+          <img src="https://img.icons8.com/office/30/000000/wet.png" />
         </Link>
+
         <Link to="/profile">Profile</Link>
         <Link to="/signout">Sign out</Link>
-        <Link to="#about">About</Link>
-        <a href="javascript:void(0);" className="icon" onClick={() => this.toggle()}>
+
+        <a
+          href="javascript:void(0);"
+          className="icon"
+          onClick={() => this.toggle()}
+        >
           <i className="fa fa-bars" />
         </a>
       </div>
