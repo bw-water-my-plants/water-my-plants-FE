f0802a70d06ff86bde3428fbacc28166728b9b16
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-27 15:25:36 +0100

    Add image list

---
+++ water-plants/src/components/Images/index.js
@@ -0,0 +1,19 @@
+import img1 from './1.svg';
+import img2 from './2.svg';
+import img3 from './3.svg';
+import img4 from './4.svg';
+import img5 from './5.svg';
+import img6 from './6.svg';
+import img7 from './7.svg';
+
+const items = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+];
+  
+export default items;
